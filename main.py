2037d7871c2422f93968883ebbfc196ab642b3b9
ghostfoxgoddes3 <al.gabriely.rocha@impatech.org.br>
2024-07-17 11:32:59 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import PROVA_1a038a685a837a770
+
+code = PROVA_1a038a685a837a770.my_code()
+name = PROVA_1a038a685a837a770.my_name()
+f = PROVA_1a038a685a837a770.Forca(1.0,0.3)
+b = PROVA_1a038a685a837a770.Bloco(10, -34, 40)
+
+print(name)
+print(code)
+print(f)
+print(b)
